4fd3d0a2e3134aef7b71645adf14f4f757633d80
elcharitas <jonathanirhodia@gmail.com>
2021-06-14 22:51:49 +0100

    Modify Home page styles location

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,125 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import {
+  IonBadge,
+  IonButton,
+  IonCard,
+  IonCardContent,
+  IonCardHeader,
+  IonCardSubtitle,
+  IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonLabel,
+  IonPage,
+  IonRoute,
+  IonRouterContext,
+  IonRouterOutlet,
+  IonSearchbar,
+  IonTabBar,
+  IonTabButton,
+  IonTabs,
+  IonTitle,
+  IonToolbar,
+} from "@ionic/react";
+import { Component, useState } from "react";
+import ExploreContainer from "../components/ExploreContainer";
+import {
+  menuSharp,
+  arrowForward,
+  alarm,
+  calendar,
+  informationCircleOutline,
+  map,
+  homeOutline,
+} from "ionicons/icons";
+import "../theme/Home.css";
+import { IonReactRouter } from "@ionic/react-router";
+import { Route } from "react-router";
+import Study from "./Study";
+import Welcome from "./Welcome";
 
-const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
+export default class Home extends Component<{}, { searchText: string }> {
+  public constructor(props = {}) {
+    super(props);
+    this.state = {
+      searchText: "",
+    };
+  }
+  public render() {
+    return (
+      <IonPage>
+        <IonHeader>
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonIcon
+              icon={menuSharp}
+              slot="start"
+              style={{ padding: "1.2em" }}
+              size="small"
+            />
+            <IonIcon
+              icon={alarm}
+              slot="end"
+              style={{ padding: "1.2em" }}
+              size="small"
+            />
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+        <IonContent>
+          <IonCard color="tertiary">
+            <IonCardHeader>
+              <IonCardSubtitle>ASFFUTA</IonCardSubtitle>
+              <IonCardTitle>Bible Study</IonCardTitle>
+            </IonCardHeader>
 
-export default Home;
+            <IonCardContent>
+              Keep close to Nature's heart... and break clear away, once in
+              awhile, and climb a mountain or spend a week in the woods. Wash
+              your spirit clean.
+            </IonCardContent>
+          </IonCard>
+
+          <IonSearchbar
+            value={this.state.searchText}
+            onIonChange={(e) => this.setState({ searchText: e.detail.value! })}
+            showCancelButton="focus"
+          ></IonSearchbar>
+          <IonCard>
+            <IonItem>
+              <IonLabel>Study 1: kgjghgc</IonLabel>
+              <IonButton fill="outline" slot="end">
+                <IonIcon icon={arrowForward} slot="end" />
+              </IonButton>
+            </IonItem>
+
+            <IonCardContent>
+              This is content, without any paragraph or header tags, within an
+              ion-cardContent element.
+            </IonCardContent>
+          </IonCard>
+          <IonButton  color="dark" autoCapitalize="true">
+            <IonLabel>Get Started</IonLabel>
+          </IonButton>
+        </IonContent>
+
+        <IonTabs>
+          <IonRouterOutlet></IonRouterOutlet>
+          <IonTabBar slot="bottom">
+            <IonTabButton tab="schedule">
+              <IonIcon icon={calendar} color="primary" />
+              <IonBadge color="warning">6</IonBadge>
+            </IonTabButton>
+
+            <IonTabButton tab="speakers">
+              <IonIcon icon={homeOutline} />
+            </IonTabButton>
+
+            <IonTabButton tab="about">
+              <IonIcon icon={informationCircleOutline} />
+            </IonTabButton>
+          </IonTabBar>
+        </IonTabs>
+      </IonPage>
+    );
+  }
+}
