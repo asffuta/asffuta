aec563fbe1f37ce4fa44b640fbfae61452f131a5
elcharitas <jonathanirhodia@gmail.com>
2021-06-15 04:48:29 +0100

    Add Fonts and Images

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,9 +1,22 @@
-import { IonContent, IonPage, IonTitle } from "@ionic/react";
+import {
+  IonButton,
+  IonContent,
+  IonIcon,
+  IonImg,
+  IonPage,
+  IonTitle,
+} from "@ionic/react";
 import { Component } from "react";
 import "../theme/Welcome.css";
 
 export default class Welcome extends Component {
   public render() {
-    return <div></div>;
+    return (
+      <IonPage>
+        <IonContent className="content" color="warning">
+          <IonButton color="dark">Get Started</IonButton>
+        </IonContent>
+      </IonPage>
+    );
   }
 }
