55931298b0ae4805c6a330ae25abc637f3041f83
elcharitas <jonathanirhodia@gmail.com>
2021-06-19 15:30:45 +0100

    Update Study page

--- src/pages/Study.tsx
+++ src/pages/Study.tsx
@@ -1,9 +1,13 @@
-import { IonContent, IonPage, IonTitle } from "@ionic/react";
+import { IonContent } from "@ionic/react";
 import { Component } from "react";
 import "../theme/Study.css";
 
 export default class Study extends Component {
   public render() {
-    return <div></div>;
+    return (
+      <IonContent>
+        
+      </IonContent>
+    );
   }
 }
