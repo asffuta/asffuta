cefb5b149cdf7ec37758ebc17377c388a08fc6b2
elcharitas <jonathanirhodia@gmail.com>
2021-06-25 12:52:07 +0100

    Fix interactivity of TopCard

--- src/components/TopCard.tsx
+++ src/components/TopCard.tsx
@@ -1,8 +1,23 @@
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCol, IonLabel, IonRow } from "@ionic/react";
 import { Component } from "react";
 import { CgCircleci } from "react-icons/cg";
+import { datef } from "../utils";
 
-export default class TopCard extends Component<HomeState> {
+export default class TopCard extends Component<{}, HomeState> {
+  constructor(props ={}){
+    super(props);
+    this.state = {}
+  }
+  
+  public componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        curDate: datef("LLLL d, y"),
+        curTime: datef("t a"),
+      });
+    });
+  }
+
   public render() {
     return (
       <IonCard color="warning" className="top-card card-radius">
@@ -11,17 +26,17 @@ export default class TopCard extends Component<HomeState> {
           <IonCardSubtitle className="small">
             Bible Study Outline:
           </IonCardSubtitle>
-          <IonLabel className="bold">A Balanced Church!</IonLabel>
+          <IonLabel className="bold">A BALANCED CHURCH!</IonLabel>
         </IonCardHeader>
         <IonCardContent>
           <IonRow>
             <IonCol size="12">
               <IonLabel>
-                <b>Date</b>: {this.props.curDate}
+                <b>Date</b>: {this.state.curDate}
                 <br />
               </IonLabel>
               <IonLabel>
-                <b>Time</b>: {this.props.curTime}
+                <b>Time</b>: {this.state.curTime}
               </IonLabel>
             </IonCol>
           </IonRow>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,24 +4,9 @@ import TopCard from "../components/TopCard";
 import StudyCards from "../components/StudyCards";
 import HeaderMenu from "../components/HeaderMenu";
 import { IonContent, IonPage } from "@ionic/react";
-import { datef, setStateInterval } from "../utils";
 import "../theme/Home.css";
 
-export default class Home extends Component<{}, HomeState> {
-  public constructor(props = {}) {
-    super(props);
-    this.state = {
-      searchText: "",
-    };
-  }
-
-  public componentDidMount() {
-    setStateInterval(this, {
-      curDate: datef("LLLL d, y"),
-      curTime: datef("t a"),
-    });
-  }
-
+export default class Home extends Component {
   public render() {
     return (
       <IonPage>
