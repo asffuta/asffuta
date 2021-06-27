cbb385a1603d30d55372aa4669534b89bc476e52
elcharitas <jonathanirhodia@gmail.com>
2021-06-27 09:17:56 +0100

    Change app name

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,10 +1,10 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.stacksail.asffuta',
-  appName: 'asffuta',
-  webDir: 'build',
-  bundledWebRuntime: true
+  appId: "com.stacksail.asffuta",
+  appName: "ASFFUTA Bible Study Outline",
+  webDir: "build",
+  bundledWebRuntime: true,
 };
 
 export default config;
